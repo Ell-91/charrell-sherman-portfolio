7940ccb4b76fb4f557d508e27737a6867efad12c
ell-91 <cdsherman@ucdavis.edu>
2024-10-25 22:33:08 -0700

    Home page text and styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,6 @@
-// import Link from "next/link";
-// import { Button } from "./ui/button";
-// import { Download, Send } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Download, Send } from "lucide-react";
 
 import {
   RiBracesFill,
@@ -11,7 +11,7 @@ import {
 
 // import DevImg from "./DevImg";
 // import Badge from "./Badge2";
-// import Socials from "./Socials";
+import Socials from "./Socials";
 
 const Hero = () => {
   return (
@@ -19,10 +19,12 @@ const Hero = () => {
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* text */}
-          <div>
-            <div>Software Engineer</div>
-            <h1 className="h1">Hello, my name is Charrell Sherman</h1>
-            <p>
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Software Engineer
+            </div>
+            <h1 className="h1 mb-4">Hello, my name is Charrell Sherman</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
               {" "}
               Passion for education and a strong dedication to delivering
               projects that make a societal impact. Skilled in modern web
@@ -32,9 +34,27 @@ const Hero = () => {
               cross-functionally with product owners, engineers, UX/UI and QA to
               define, design, and new features.
             </p>
-            {/* image */}
-            <div>image</div>
+            {/* button */}
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  Contact Charrell
+                  <Send size={18} />
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2">
+                Download Resume
+                <Download size={18} />
+              </Button>
+            </div>
+            {/* socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
           </div>
+          {/* image */}
+          <div className="hidden xl:flex relative">image</div>
           {/* icons */}
           <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
             <RiArrowDownSLine className="text-3xl text-primary" />
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
 
-const Socials = () => {
+import {
+  RiYoutubeFill,
+  RiLinkedinFill,
+  RiGithubFill,
+  RiInstagramFill,
+} from "react-icons/ri";
+
+import Link from "next/link";
+
+import React from "react";
+const icons = [
+  {
+    path: "/",
+    name: <RiYoutubeFill />,
+  },
+  {
+    path: "/",
+    name: <RiLinkedinFill />,
+  },
+  {
+    path: "/",
+    name: <RiGithubFill />,
+  },
+  {
+    path: "/",
+    name: <RiInstagramFill />,
+  },
+];
+
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
-    <div>Socials</div>
-  )
-}
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link href={icon.path} key={index}>
+            <div className={`${iconStyles}`}>{icon.name}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Socials
+export default Socials;

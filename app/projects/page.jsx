2d8d0c5a95c0faad0832de68adf77ffed526f8d4
ell-91 <cdsherman@ucdavis.edu>
2024-11-01 03:28:03 -0400

    Contact me page complete

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,146 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
+
+const projectData = [
+  {
+    image: "/work/3.png",
+    category: "react js",
+    name: "Blah Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "next js",
+    name: "Hah Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Cacao Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "next js",
+    name: "Charrell Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "Events Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "next js",
+    name: "Wrapper Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "fullstack",
+    name: "Lavish Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "fullstack",
+    name: "Nova Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "fullstack",
+    name: "Nintique Website",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    link: "/",
+    github: "/",
+  },
+];
+
+const uniqueCategories = [
+  "all projects",
+  ...new Set(projectData.map((item) => item.category)),
+];
 
 const Projects = () => {
-  return <div>Project Page</div>;
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [category, setCategory] = useState("all projects");
+
+  // if category is 'all projects' return all projects, else filter by category
+  const filteredProjects = projectData.filter((project) => {
+    return category === "all projects"
+      ? project
+      : project.category === category;
+  });
+
+  return (
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          My Project
+        </h2>
+        <Tabs defaultValue={category} className="m-24 xl:mb-48">
+          <TabsList
+            className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] 
+          mb-12 mx-auto md:border dark:border-none"
+          >
+            {categories.map((category, index) => {
+              return (
+                <TabsTrigger
+                  onClick={() => setCategory(category)}
+                  value={category}
+                  key={index}
+                  className="capitalize w-[162px] md:w-auto"
+                >
+                  {category}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          {/* tabs content */}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;

ed3a382cab1d5a8b34bdb49a9ed68c2db5241bfa
ell-91 <cdsherman@ucdavis.edu>
2024-11-23 14:05:04 -0500

    Page layout/size

--- components/Services.jsx
+++ components/Services.jsx
@@ -38,6 +38,7 @@ const serviceData = [
 
 const Services = () => {
   return (
+    // mb-12 xl:mb-36 pb-12 xl:pb-24
     <section className="mb-12 xl:mb-36">
       <div className="container mx-auto">
         <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">
@@ -49,7 +50,8 @@ const Services = () => {
             const IconComponent = iconMap[item.icon]; // Use the icon name to get the corresponding component
             return (
               <Card
-                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                // "w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                className="w-full max-w-[424px] min-h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
                 key={index}
               >
                 <CardHeader className="text-primary absolute -top-[60px]">
